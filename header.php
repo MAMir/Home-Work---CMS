beac1e36c978a7718f2317e829f3b5981f3b07bb
Seyed Mohammad Ali Mirrezaei <m.a.mirrezaee@gmail.com>
2013-07-11 16:19:40 +0430

    Update header.php

---
+++ header.php
@@ -0,0 +1,7 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title><?php bloginfo('name'); wp_title(); ?></title>
+<link href="<?php bloginfo('template_url'); ?>\style.css" rel="stylesheet" type="text/css">
+</head>
